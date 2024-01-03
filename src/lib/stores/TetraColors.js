e0962ff74f64732551f52fc0c937e77a11a68a12
Hanz Felix Tumulak <tumulakhanz@gmail.com>
2024-01-03 20:51:34 +0800

    Delete remove() function

--- src/lib/stores/TetraColors.js
+++ src/lib/stores/TetraColors.js
@@ -19,14 +19,26 @@ function createTetraColorStore() {
 			update((tetracolors) => (tetracolors[uid++] = color));
 		},
 		init: (initialcolors) => {
-			initialcolors[0] = configColors.tbrown['50'];
-			initialcolors['X'] = configColors.tcyan['400'];
-			set(initialcolors);
-			uid = 1 + Object.keys(initialcolors).length;
-		},
-		remove: (tetrapiece) => {
-			update(($tetrapieces) => $tetrapieces.filter((t) => t !== tetrapiece));
+			if (initialcolors instanceof Array) {
+				uid = 1;
+				let colors = {
+					0: configColors.tbrown['50'],
+					X: configColors.tcyan['400']
+				};
+				for (const color of initialcolors) {
+					colors[uid++] = color;
+				}
+				set(colors);
+			} else if (initialcolors instanceof Object) {
+				initialcolors[0] = configColors.tbrown['50'];
+				initialcolors['X'] = configColors.tcyan['400'];
+				uid = 1 + Object.keys(initialcolors).length;
+				set(initialcolors);
+			} else {
+				// throw an error?
+			}
 		}
+		// TODO: how to implement remove:
 	};
 }
 
