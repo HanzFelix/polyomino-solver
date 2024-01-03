1d24602fd4bb5758183b3ae232890b5b83b12ac6
Hanz Felix Tumulak <tumulakhanz@gmail.com>
2024-01-03 22:18:06 +0800

    Change adapter from static

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,4 @@
-import adapter from '@sveltejs/adapter-static';
+import adapter from '@sveltejs/adapter-clouflare';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -8,13 +8,11 @@ const config = {
 		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
 		adapter: adapter({
-			pages: 'build',
-			assets: 'build',
-			fallback: '404.html',
-			precompress: false,
-			strict: true
+			routes: {
+				include: ['/*'],
+				exclude: ['<all>']
+			}
 		}),
-		paths: { base: process.argv.includes('dev') ? '' : process.env.BASE_PATH },
 		alias: { tailwindConfig: 'tailwind.config.js' }
 	},
 	preprocess: vitePreprocess()
