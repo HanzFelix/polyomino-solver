1db1ab180c1d274c99ac5e182e71b59dff1f9be7
Hanz Felix Tumulak <tumulakhanz@gmail.com>
2024-01-04 15:59:01 +0800

    Update layout

--- src/lib/TetraPieceList.svelte
+++ src/lib/TetraPieceList.svelte
@@ -1,5 +1,5 @@
 <script>
-	import { onMount } from 'svelte';
+	import { tetrapieces } from '$lib/stores/TetraPieces.js';
 	import TetraRender from '$lib/TetraRender.svelte';
 
 	export let pieces;
@@ -7,7 +7,7 @@
 
 <div class="grid grid-cols-4 sm:grid-cols-6 md:grid-cols-3 lg:grid-cols-4 gap-4 items-end">
 	{#each pieces as piece}
-		<div class="flex flex-col justify-end gap-2">
+		<div class="flex flex-col justify-end gap-1 rounded-t-md">
 			<TetraRender shape={piece.shape} grid={4} />
 			<input
 				type="text"
@@ -22,10 +22,11 @@
 					if (piece.quantity == '') {
 						piece.quantity = 0;
 					}
+					tetrapieces.refresh();
 				}}
 				placeholder="0"
 				bind:value={piece.quantity}
-				class="px-2 basis-full bg-tbrown-50 rounded-md text-center {piece.quantity <= 0
+				class="px-2 basis-full bg-tbrown-50 rounded-b-md text-center {piece.quantity <= 0
 					? 'text-tbrown-500'
 					: 'text-tbrown-900'}"
 			/>
