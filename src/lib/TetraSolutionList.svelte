37e73f997ad22ad8e30014c56302b074385e62c0
Hanz Felix Tumulak <tumulakhanz@gmail.com>
2024-01-05 19:10:34 +0800

    Add popup preview on click

--- src/lib/TetraSolutionList.svelte
+++ src/lib/TetraSolutionList.svelte
@@ -1,14 +1,43 @@
 <script>
 	import TetraRender from '$lib/TetraRender.svelte';
 	export let solutions;
+	let solutionPreview;
+	let openedSolution = [[]];
+
+	function previewSolution(index) {
+		openedSolution = solutions[index];
+		solutionPreview.showModal();
+	}
 </script>
 
 <div
-	class="grid overflow-x-auto py-2 pr-8 md:pr-0 md:overflow-x-visible grid-rows-2 md:grid-rows-none md:grid-cols-2 grid-flow-col md:grid-flow-row md:overflow-y-auto gap-4 justify-start"
+	class="grid overflow-x-auto py-2 pr-8 pl-4 md:pr-0 md:overflow-x-visible grid-rows-2 md:grid-rows-none md:grid-cols-2 grid-flow-col md:grid-flow-row md:overflow-y-auto gap-4 justify-start"
 >
-	{#each solutions as solution}
-		<div class="w-20 md:w-auto rounded-sm">
+	{#each solutions as solution, i}
+		<button
+			class="w-20 md:w-auto rounded-sm"
+			on:click={() => {
+				previewSolution(i);
+			}}
+		>
 			<TetraRender shape={solution} grid={4} />
-		</div>
+		</button>
 	{/each}
 </div>
+
+<dialog
+	bind:this={solutionPreview}
+	class="rounded-lg backdrop:backdrop-brightness-50 bg-tbrown-500 w-full max-w-lg overflow-y-auto"
+>
+	<div class="flex justify-end">
+		<button
+			class="material-symbols-rounded font-black text-tbrown-50 bg-tcyan-900 px-8 py-3 mr-2"
+			on:click={() => {
+				solutionPreview.close();
+			}}>close</button
+		>
+	</div>
+	<div class="rounded-lg aspect-square ml-4 mb-4">
+		<TetraRender shape={openedSolution} radius={1} />
+	</div>
+</dialog>
