2cd6b6f89f62d43a5255c0bb5b17bacc6d8c7437
Hanz Felix Tumulak <tumulakhanz@gmail.com>
2024-05-04 11:21:27 +0800

    Add website title

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -215,12 +215,33 @@
 </script>
 
 <svelte:head>
-	<title>Tetra Solver</title>
+	<title>Polyomino Solver</title>
 	<meta name="description" content="Tetra Solver" />
 </svelte:head>
 <div class="container mx-auto flex flex-col md:flex-row gap-4 py-8 px-4 md:px-0 h-full">
 	<div class="md:basis-4/12 flex flex-col justify-between bg-tbrown-300 rounded-xl">
 		<div class="flex gap-6 flex-col p-4 md:pr-2 bg-tbrown-300 rounded-t-lg md:overflow-y-auto">
+			<header class="flex justify-between">
+				<h1 class="text-xl font-bold">Polyomino Solver</h1>
+				<a
+					href="https://github.com/HanzFelix/polyomino-solver"
+					class="fill-tbrown-900 transition-colors hover:fill-tbrown-50"
+				>
+					<svg
+						height="24"
+						aria-hidden="true"
+						viewBox="0 0 16 16"
+						version="1.1"
+						width="24"
+						data-view-component="true"
+						class="octicon octicon-mark-github"
+					>
+						<path
+							d="M8 0c4.42 0 8 3.58 8 8a8.013 8.013 0 0 1-5.45 7.59c-.4.08-.55-.17-.55-.38 0-.27.01-1.13.01-2.2 0-.75-.25-1.23-.54-1.48 1.78-.2 3.65-.88 3.65-3.95 0-.88-.31-1.59-.82-2.15.08-.2.36-1.02-.08-2.12 0 0-.67-.22-2.2.82-.64-.18-1.32-.27-2-.27-.68 0-1.36.09-2 .27-1.53-1.03-2.2-.82-2.2-.82-.44 1.1-.16 1.92-.08 2.12-.51.56-.82 1.28-.82 2.15 0 3.06 1.86 3.75 3.64 3.95-.23.2-.44.55-.51 1.07-.46.21-1.61.55-2.33-.66-.15-.24-.6-.83-1.23-.82-.67.01-.27.38.01.53.34.19.73.9.82 1.13.16.45.68 1.31 2.69.94 0 .67.01 1.3.01 1.49 0 .21-.15.45-.55.38A7.995 7.995 0 0 1 0 8c0-4.42 3.58-8 8-8Z"
+						></path>
+					</svg>
+				</a>
+			</header>
 			<section>
 				<div class="flex justify-between mb-2 items-end flex-wrap">
 					<h2 class="text-xl">Board size</h2>
