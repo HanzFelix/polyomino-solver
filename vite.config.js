565e6fd36c1d3e52d79484d4867939a9f19861c2
Hanz Felix Tumulak <tumulakhanz@gmail.com>
2025-05-12 10:58:38 +0800

    Add vite-plugin-devtools-json dependency

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,10 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 import { searchForWorkspaceRoot } from 'vite';
+import devtoolsJson from 'vite-plugin-devtools-json';
 
 export default defineConfig({
-	plugins: [sveltekit()],
+	plugins: [sveltekit(), devtoolsJson()],
 	server: {
 		fs: {
 			allow: [searchForWorkspaceRoot(process.cwd())]
