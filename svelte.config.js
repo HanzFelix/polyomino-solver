2f4a28505bfef5994ec8fe8b3cf6cd4e0148545a
Hanz Felix Tumulak <tumulakhanz@gmail.com>
2026-01-22 18:49:42 +0800

    refactor: remove unused options

--- svelte.config.js
+++ svelte.config.js
@@ -2,19 +2,6 @@ import adapter from '@sveltejs/adapter-cloudflare';
 
 export default {
 	kit: {
-		adapter: adapter({
-			// See below for an explanation of these options
-			config: undefined,
-			platformProxy: {
-				configPath: undefined,
-				environment: undefined,
-				persist: undefined
-			},
-			fallback: 'plaintext',
-			routes: {
-				include: ['/*'],
-				exclude: ['<all>']
-			}
-		})
+		adapter: adapter()
 	}
 };
